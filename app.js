cde0b32971514a049ef8f60f225750e73c427466
Ubuntu <ubuntu@ip-172-31-24-207.ap-northeast-1.compute.internal>
2019-04-20 08:46:59 +0000

    commit

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
+var express = require('express');
+var app = express();
 
 var io = require('socket.io')(3000);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/rooms', function(req, res){
+  const response = [
+    {
+      "id": 1,
+      "name": "信長交換部屋！",
+      "isFull": false
+    },
+    {
+      "id": 2,
+      "name": "Commonください！",
+      "isFull": false
+    },
+    {
+      "id": 3,
+      "name": "Rare交換部屋",
+      "isFull": true
+    }
+  ]
+
+  res.send(response);
+})
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+//===========SocketIO===============
 
 /**
  * connection
@@ -26,6 +67,8 @@ io.sockets.on('connection', function (socket) {
    * メッセージを挿入
    */
   socket.on('insert:message', function (data) {
+    console.log(data)
+    console.log(socket.data)
     io.sockets.to(socket.data.room).emit('insert:message', data.message);
   });
 
